4bd8f258a9c0f74dd945caf8aabe76b4aa7bbcd7
Ramin Karimpor <ramin.kp81@gmail.com>
2023-09-18 23:28:57 +0330

    Prject Config

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function App() {
   return (
-    <div className='bg-rose-500'>App</div>
+    <div>
+      <h1 className='font-Dana'>سلام وقت بخیر و عرض ادب</h1>
+      <h1 className='font-DanaMedium'>سلام وقت بخیر و عرض ادب</h1>
+      <h1 className='font-DanaBold'>سلام وقت بخیر و عرض ادب</h1>
+      <h1 className='font-MorabbaLight'>سلام وقت بخیر و عرض ادب</h1>
+      <h1 className='font-MorabbaMedium'>سلام وقت بخیر و عرض ادب</h1>
+      <h1 className='font-MorabbaBold'>سلام وقت بخیر و عرض ادب</h1>
+    </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,29 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brown: {
+          100: "#ECE0D1",
+          300: "#DBC1AC",
+          600: "#967259",
+        },
+      },
+      boxShadow: {
+        "shadow-normal": "0px 1px 10px rgba(0, 0, 0, 0.05)",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+      fontFamily: {
+        Dana: "Dana",
+        DanaMedium: "Dana Medium",
+        DanaBold: "Dana Bold",
+        MorabbaLight: "Morabba Light",
+        MorabbaMedium: "Morabba Medium",
+        MorabbaBold: "Morabba Bold",
+      },
+    },
   },
   plugins: [],
 };
