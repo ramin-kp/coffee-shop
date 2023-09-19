a1af9faa3a246067cfb4ee668bf533365a186183
Ramin Karimpor <ramin.kp81@gmail.com>
2023-09-19 04:18:14 +0330

    feat: add svg icone

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,27 +6,65 @@ export default function Header() {
     <header>
       <div>
         <div>
-          <Link to="/">
-            <img src="/images/svgs/app-logo.svg" alt="goldenLog" />
+          <div>
+            <Link to="/">
+              <img src="/images/svgs/app-logo.svg" alt="goldenLog" />
+            </Link>
+          </div>
+          <ul>
+            <li>
+              <Link to="#">صفحه اصلی</Link>
+            </li>
+            <li>
+              <Link to="#">فروشگاه</Link>
+              <ul>
+                <li>
+                  <Link to="#">قهوه ترک</Link>
+                </li>
+                <li>
+                  <Link to="#">قهوه اسپرسو</Link>
+                </li>
+                <li>
+                  <Link to="#">قهوه فوری</Link>
+                </li>
+                <li>
+                  <Link to="#">قهوه فرانسه</Link>
+                </li>
+                <li>
+                  <Link to="#">کپسول اسپرسو</Link>
+                </li>
+                <li>
+                  <Link to="#">لوازم جانبی و تجهیزات</Link>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <Link to="#">مقالات</Link>
+            </li>
+            <li>
+              <Link to="#">درباره ما</Link>
+            </li>
+            <li>
+              <Link to="#">تماس باما</Link>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <div>
+            <svg>
+              <use xlinkHref="#shoping-card"></use>
+            </svg>
+            <svg>
+              <use xlinkHref="#moon"></use>
+            </svg>
+          </div>
+          <Link to="#">
+            ورود|ثبت‌ نام
+            <svg>
+              <use xlinkHref="#arrow-right-on-rectangle"></use>
+            </svg>
           </Link>
         </div>
-        <ul>
-          <li>صفحه اصلی</li>
-          <li>فروشگاه</li>
-          <li>مقالات</li>
-          <li>درباره ما</li>
-          <li>تماس باما</li>
-        </ul>
-      </div>
-      <div>
-        <div>
-          <svg></svg>
-          <svg></svg>
-        </div>
-        <Link to="#">
-          ورود|ثبت‌ نام
-          <svg></svg>
-        </Link>
       </div>
     </header>
   );
