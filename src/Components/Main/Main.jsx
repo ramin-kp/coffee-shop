54a12b4daf8440dc050acf6a12493a4aad6a16cc
Ramin Karimpor <ramin.kp81@gmail.com>
2023-09-28 11:57:10 +0330

    fix: fixed lineHeight for home text

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -6,10 +6,10 @@ export default function Main() {
       <section className="home relative bg-home-mobile h-[200px] xs:h-auto xs:aspect-[2/1] md:aspect-auto md:bg-home-desktop bg-no-repeat bg-cover bg-[center_top]">
         <div className="container relative flex h-full overflow-y-hidden sm:h-auto items-center justify-end md:min-h-screen ">
           <div className="text-white">
-            <h2 className="mb-0.5 font-MorabbaBold text-2xl md:mb-2 md:text-6xl">
+            <h2 className="mb-0.5 font-MorabbaBold text-2xl md:mb-2 md:text-6xl/[62px]">
               قهوه عربیکا تانزانیکا
             </h2>
-            <span className="block font-MorabbaLight text-xl md:text-5xl">
+            <span className="block font-MorabbaLight text-xl md:text-5xl/[64px]">
               یک فنجان بالانس
             </span>
             <div className="w-[100px] h-0.5 my-3 md:my-8 bg-orange-300"></div>
