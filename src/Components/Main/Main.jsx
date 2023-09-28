a4952270e8bfdd984faeedaf9ec22dfc5abf5ecb
Ramin Karimpor <ramin.kp81@gmail.com>
2023-09-28 12:01:31 +0330

    fix: fixed icone home section

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -32,8 +32,8 @@ export default function Main() {
         </svg>
         {/* arrow circle */}
         <div className="absolute left-0 right-0 mx-auto hidden md:flex items-center justify-center w-[30px] h-[30px] bottom-0 translate-y-2/4  border-2 rounded-full border-orange-300">
-          <svg className="w-4 h-4 text-zinc-700 dark:text-white">
-            <use href="#chevron-down"></use>
+          <svg className="w-5 h-5 text-zinc-700 dark:text-white">
+            <use href="#chevron-down-mini"></use>
           </svg>
         </div>
       </section>
