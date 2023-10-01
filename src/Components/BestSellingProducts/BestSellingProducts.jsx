097029fe410f1cb30734f0a403b605e6945c2d1a
Ramin Karimpor <ramin.kp81@gmail.com>
2023-10-01 13:44:32 +0330

    feat: delope best selling component

--- src/Components/BestSellingProducts/BestSellingProducts.jsx
+++ src/Components/BestSellingProducts/BestSellingProducts.jsx
@@ -1,168 +1,38 @@
 import React from "react";
 import SectionHeader from "../SectionHeader/SectionHeader";
-import CategoryBanner from "../CategoryBanner/CategoryBanner";
-import ProductCategory from "../ProductCategory/ProductCategory";
-import BestSellingProducts from "../BestSellingProducts/BestSellingProducts";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
 
-export default function Main() {
+export default function BestSellingProducts() {
   return (
-    <main>
-      <section className="home relative bg-home-mobile h-[200px] xs:h-auto xs:aspect-[2/1] md:aspect-auto md:bg-home-desktop bg-no-repeat bg-cover bg-[center_top]">
-        <div className="container relative flex h-full overflow-y-hidden sm:h-auto items-center justify-end md:min-h-screen ">
-          <div className=" text-white">
-            <h2 className="mb-0.5 font-MorabbaBold text-2xl md:mb-2 md:text-6xl/[62px]">
-              قهوه عربیکا تانزانیکا
-            </h2>
-            <span className="block font-MorabbaLight text-xl md:text-5xl/[64px]">
-              یک فنجان بالانس
-            </span>
-            <div className="w-[100px] h-0.5 my-3 md:my-8 bg-orange-300"></div>
-            <p className="text-xs max-w-[201px]  md:max-w-[460px] md:text-xl">
-              قطعانام آشنای عربیکا را شنیده اید، عربیکا یکی از گونه های قهوه است
-              که در نواحی مختلف کمربند قهوه کشت میشود.
-            </p>
-          </div>
-          {/* circle */}
-          <div className="circle circle-main circle-lg">
-            <div className="circle circle-md ">
-              <div className="circle circle-sm "></div>
-            </div>
-          </div>
-        </div>
-
-        {/* curve */}
-        <svg className="hidden md:block absolute bottom-0 left-0 right-0 w-[100px] h-[22px] mx-auto text-gray-100 dark:text-zinc-800">
-          <use href="#curve"></use>
-        </svg>
-        {/* arrow circle */}
-        <div className="absolute left-0 right-0 mx-auto hidden md:flex items-center justify-center w-[30px] h-[30px] bottom-0 translate-y-2/4  border-2 rounded-full border-orange-300">
-          <svg className="w-5 h-5 text-zinc-700 dark:text-white">
-            <use href="#chevron-down-mini"></use>
-          </svg>
-        </div>
-      </section>
-      <section className="products pt-8 md:pt-24 lg:pt-48">
-        <div className="container">
-          <SectionHeader
-            titleHeader="جدیدترین محصولات"
-            subTitleHeader="فرآوری شده از دانه قهوه"
-            desTitle="مشاهده همه محصولات"
-            mobTitle="مشاهده همه "
-            id="#chevron-left-mini"
-          />
-          {/* products grid */}
-          <div className="grid gap-5 grid-cols-2 sm:grid-cols-3 lg:grid-cols-4">
-            <div className="p-5 bg-white dark:bg-zinc-700 rounded-2xl shadow-normal">
-              <div className="relative mb-2 md:mb-5">
-                <img
-                  src="./images/products/p1.png"
-                  className="w-32 md:w-auto"
-                  alt="p 1"
-                />
-                <span className="absolute right-1 top-1 h-[30px] font-DanaBold text-xs/[34px] md:text-base/[34px] text-white dark:text-zinc-700 bg-orange-300 rounded-full px-2.5 md:px-3.5">
-                  12%
-                </span>
-              </div>
-              <h5 className="font-DanaMedium h-10 md:h-14 text-sm md:text-xl text-zinc-700 dark:text-white line-clamp-2 ">
-                پودر قهوه اسپرسو مانوکا250 گرمی
-              </h5>
-
-              <div className="flex gap-2 md:gap-2.5 items-center mt-1.5 md:mt-2.5">
-                <div className=" text-teal-600 dark:text-emerald-500">
-                  <span className="font-DanaBold text-sm md:text-xl">
-                    220,000
-                  </span>
-                  <span className="text-sm tracking-tighter"> تومان</span>
-                </div>
-                <div className="offer">
-                  <span className="text-xs md:text-xl">175,000</span>
-                  <span className="hidden xl:inline sm:text-sm">تومان</span>
-                </div>
-              </div>
-              <div className="flex items-center justify-between mt-2.5">
-                <div className="flex gap-2.5 md:gap-3 items-center">
-                  <div className="flex-center w-[26px] h-[26px] md:w-9 md:h-9 text-gray-400 hover:text-white bg-gray-100 hover:bg-teal-600 dark:bg-zinc-800 dark:hover:bg-emerald-500 rounded-full cursor-pointer transition-all ">
-                    <svg className="w-4 h-4 md:w-[22px] md:h-[22px]">
-                      <use href="#shoping-card"></use>
-                    </svg>
-                  </div>
-                  <div className="flex-center text-gray-400  hover:text-teal-600  dark:hover:text-emerald-500 rounded-full cursor-pointer transition-all ">
-                    <svg className="w-4 h-4 md:w-6 md:h-6">
-                      <use href="#arrows-right-left"></use>
-                    </svg>
-                  </div>
-                </div>
-                <div className="flex text-yellow-400">
-                  <svg className="w-4 h-4 md:w-6 md:h-6 text-gray-300 dark:text-gray-400  ">
-                    <use href="#star"></use>
-                  </svg>
-                  <svg className="w-4 h-4 md:w-6 md:h-6 ">
-                    <use href="#star"></use>
-                  </svg>
-                  <svg className="w-4 h-4 md:w-6 md:h-6 ">
-                    <use href="#star"></use>
-                  </svg>
-                  <svg className="w-4 h-4 md:w-6 md:h-6 ">
-                    <use href="#star"></use>
-                  </svg>
-                  <svg className="w-4 h-4 md:w-6 md:h-6 ">
-                    <use href="#star"></use>
-                  </svg>
-                </div>
-              </div>
-            </div>
-            <div className="p-5 bg-white dark:bg-zinc-700 rounded-2xl shadow-normal">
-              <div className="relative mb-2 md:mb-5">
-                <img
-                  src="./images/products/p2.png"
-                  className="w-32 md:w-auto"
-                  alt="p 1"
-                />
-              </div>
-              <h5 className="font-DanaMedium h-10 md:h-14 text-sm md:text-xl text-zinc-700 dark:text-white line-clamp-2 ">
-                قهوه اسپرسو روسو
-              </h5>
-
-              <div className="flex gap-2 md:gap-2.5 items-center mt-1.5 md:mt-2.5">
-                <div className=" text-teal-600 dark:text-emerald-500">
-                  <span className="font-DanaBold text-sm md:text-xl">
-                    720,000
-                  </span>
-                  <span className="text-sm tracking-tighter"> تومان</span>
-                </div>
-              </div>
-              <div className="flex items-center justify-between mt-2.5">
-                <div className="flex gap-2.5 md:gap-3 items-center">
-                  <div className="flex-center w-[26px] h-[26px] md:w-9 md:h-9 text-gray-400 hover:text-white bg-gray-100 hover:bg-teal-600 dark:bg-zinc-800 dark:hover:bg-emerald-500 rounded-full cursor-pointer transition-all ">
-                    <svg className="w-4 h-4 md:w-[22px] md:h-[22px]">
-                      <use href="#shoping-card"></use>
-                    </svg>
-                  </div>
-                  <div className="flex-center text-gray-400  hover:text-teal-600  dark:hover:text-emerald-500 rounded-full cursor-pointer transition-all ">
-                    <svg className="w-4 h-4 md:w-6 md:h-6">
-                      <use href="#arrows-right-left"></use>
-                    </svg>
-                  </div>
-                </div>
-                <div className="flex text-yellow-400">
-                  <svg className="w-4 h-4 md:w-6 md:h-6 text-gray-300 dark:text-gray-400  ">
-                    <use href="#star"></use>
-                  </svg>
-                  <svg className="w-4 h-4 md:w-6 md:h-6 text-gray-300 dark:text-gray-400  ">
-                    <use href="#star"></use>
-                  </svg>
-                  <svg className="w-4 h-4 md:w-6 md:h-6 ">
-                    <use href="#star"></use>
-                  </svg>
-                  <svg className="w-4 h-4 md:w-6 md:h-6 ">
-                    <use href="#star"></use>
-                  </svg>
-                  <svg className="w-4 h-4 md:w-6 md:h-6 ">
-                    <use href="#star"></use>
-                  </svg>
-                </div>
-              </div>
-            </div>
+    <section className="pb-9 md:pb-20">
+      <div className="container">
+        <SectionHeader
+          titleHeader="پرفروش ترین محصولات"
+          subTitleHeader="پیشنهاد قهوه خور ها ..."
+          slider={true}
+        />
+        <Swiper
+          slidesPerView={2}
+          spaceBetween={14}
+          navigation={true}
+          breakpoints={{
+            640: {
+              slidesPerView: 3,
+              spaceBetween: 14,
+            },
+            768: {
+              slidesPerView: 3,
+              spaceBetween: 20,
+            },
+            1024: {
+              slidesPerView: 4,
+              spaceBetween: 20,
+            },
+          }}
+        >
+          <SwiperSlide>
             <div className="p-5 bg-white dark:bg-zinc-700 rounded-2xl shadow-normal">
               <div className="relative mb-2 md:mb-5">
                 <img
@@ -212,6 +82,8 @@ export default function Main() {
                 </div>
               </div>
             </div>
+          </SwiperSlide>
+          <SwiperSlide>
             <div className="p-5 bg-white dark:bg-zinc-700 rounded-2xl shadow-normal">
               <div className="relative mb-2 md:mb-5">
                 <img
@@ -271,6 +143,8 @@ export default function Main() {
                 </div>
               </div>
             </div>
+          </SwiperSlide>
+          <SwiperSlide>
             <div className="p-5 bg-white dark:bg-zinc-700 rounded-2xl shadow-normal">
               <div className="relative mb-2 md:mb-5">
                 <img
@@ -323,6 +197,8 @@ export default function Main() {
                 </div>
               </div>
             </div>
+          </SwiperSlide>
+          <SwiperSlide>
             <div className="p-5 bg-white dark:bg-zinc-700 rounded-2xl shadow-normal">
               <div className="relative mb-2 md:mb-5">
                 <img
@@ -382,6 +258,8 @@ export default function Main() {
                 </div>
               </div>
             </div>
+          </SwiperSlide>
+          <SwiperSlide>
             <div className="p-5 bg-white dark:bg-zinc-700 rounded-2xl shadow-normal">
               <div className="relative mb-2 md:mb-5">
                 <img
@@ -441,16 +319,21 @@ export default function Main() {
                 </div>
               </div>
             </div>
+          </SwiperSlide>
+          <SwiperSlide>
             <div className="p-5 bg-white dark:bg-zinc-700 rounded-2xl shadow-normal">
               <div className="relative mb-2 md:mb-5">
                 <img
-                  src="./images/products/p8.png"
+                  src="./images/products/p1.png"
                   className="w-32 md:w-auto"
-                  alt="p 8"
+                  alt="p 1"
                 />
+                <span className="absolute right-1 top-1 h-[30px] font-DanaBold text-xs/[34px] md:text-base/[34px] text-white dark:text-zinc-700 bg-orange-300 rounded-full px-2.5 md:px-3.5">
+                  12%
+                </span>
               </div>
               <h5 className="font-DanaMedium h-10 md:h-14 text-sm md:text-xl text-zinc-700 dark:text-white line-clamp-2 ">
-                پودر قهوه مانوکا250 گرمی
+                پودر قهوه اسپرسو مانوکا250 گرمی
               </h5>
 
               <div className="flex gap-2 md:gap-2.5 items-center mt-1.5 md:mt-2.5">
@@ -459,6 +342,10 @@ export default function Main() {
                     220,000
                   </span>
                   <span className="text-sm tracking-tighter"> تومان</span>
+                </div>
+                <div className="offer">
+                  <span className="text-xs md:text-xl">175,000</span>
+                  <span className="hidden xl:inline sm:text-sm">تومان</span>
                 </div>
               </div>
               <div className="flex items-center justify-between mt-2.5">
@@ -478,13 +365,13 @@ export default function Main() {
                   <svg className="w-4 h-4 md:w-6 md:h-6 text-gray-300 dark:text-gray-400  ">
                     <use href="#star"></use>
                   </svg>
-                  <svg className="w-4 h-4 md:w-6 md:h-6 text-gray-300 dark:text-gray-400  ">
+                  <svg className="w-4 h-4 md:w-6 md:h-6 ">
                     <use href="#star"></use>
                   </svg>
-                  <svg className="w-4 h-4 md:w-6 md:h-6 text-gray-300 dark:text-gray-400  ">
+                  <svg className="w-4 h-4 md:w-6 md:h-6 ">
                     <use href="#star"></use>
                   </svg>
-                  <svg className="w-4 h-4 md:w-6 md:h-6 text-gray-300 dark:text-gray-400  ">
+                  <svg className="w-4 h-4 md:w-6 md:h-6 ">
                     <use href="#star"></use>
                   </svg>
                   <svg className="w-4 h-4 md:w-6 md:h-6 ">
@@ -493,12 +380,9 @@ export default function Main() {
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-      </section>
-      <CategoryBanner />
-      <ProductCategory />
-      <BestSellingProducts />
-    </main>
+          </SwiperSlide>
+        </Swiper>
+      </div>
+    </section>
   );
 }
--- src/Components/SectionHeader/SectionHeader.jsx
+++ src/Components/SectionHeader/SectionHeader.jsx
@@ -7,6 +7,9 @@ export default function SectionHeader({
   desTitle,
   mobTitle,
   id,
+  slider,
+  next,
+  prev,
 }) {
   return (
     <div className="flex items-end justify-between mb-5 md:mb-12">
@@ -14,15 +17,31 @@ export default function SectionHeader({
         <h3 className="section-title">{titleHeader}</h3>
         <span className="section-usb-title">{subTitleHeader}</span>
       </div>
-      <div >
-        <Link className="section-link" to="#">
-          <span className="hidden md:inline-block">{desTitle}</span>
-          <span className="inline-block md:hidden">{mobTitle}</span>
-          <svg className="w-5 h-5">
-            <use href={id}></use>
-          </svg>
-        </Link>
-      </div>
+      {id && (
+        <div>
+          <Link className="section-link" to="#">
+            <span className="hidden md:inline-block">{desTitle}</span>
+            <span className="inline-block md:hidden">{mobTitle}</span>
+            <svg className="w-5 h-5">
+              <use href={id}></use>
+            </svg>
+          </Link>
+        </div>
+      )}
+      {slider && (
+        <div className="flex gap-3 md:gap-[18px] items-center">
+          <div className="flex-center w-9 md:w-10 h-9 md:h-10 bg-white dark:bg-zinc-700 text-zinc-700 dark:text-white hover:bg-gray-300 hover:dark:bg-white hover:dark:text-zinc-700 shadow-normal rotate-180 rounded-full transition-all cursor-pointer">
+            <svg className="w-6 h-6">
+              <use href="#chevron-left-mini"></use>
+            </svg>
+          </div>
+          <div className="flex-center w-9 md:w-10 h-9 md:h-10 bg-white dark:bg-zinc-700 text-zinc-700 dark:text-white hover:bg-gray-300 hover:dark:bg-white hover:dark:text-zinc-700 shadow-normal rounded-full transition-all cursor-pointer">
+            <svg className="w-6 h-6">
+              <use href="#chevron-left-mini"></use>
+            </svg>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
